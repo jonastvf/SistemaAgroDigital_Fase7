eb3c9b1719d0bbd25f1e3837b89b9c12c01e50cd
Raphael Dinelli <raphael.dinelli14@gmail.com>
2025-11-11 23:47:06 -0300

    feat(planting_calc_area_controller): Implementing controller

---
+++ src/app/controller/planting_calc_area_controller.py
@@ -0,0 +1,17 @@
+import os
+
+from app.db.connection.sql_alchemy import SqlAlchemyBuilder
+from app.services.planting_service import PlantingService
+
+
+class PlantingCalcAreaController:
+    sql_server   = os.getenv('MYSQL_HOST')
+    sql_database = os.getenv('MYSQL_DATABASE')
+    db_builder = SqlAlchemyBuilder(sql_server, sql_database)
+    session_local = db_builder.get_session()
+
+    def get_cultures(self):
+        return PlantingService.get_cultures(self.session_local())
+
+    def get_calcs(self):
+        return PlantingService.get_calcs(self.session_local())
--- src/app/routes/api.py
+++ src/app/routes/api.py
@@ -1,7 +1,77 @@
 from flask import Blueprint, request, jsonify
+import os
 
+from sqlalchemy import select
+
+from app.db.connection.sql_alchemy import SqlAlchemyBuilder
+from app.db.models.culture_model import Culture
+from app.services.planting_service import PlantingService
 api_bp = Blueprint('api', __name__, url_prefix='/api')
+
+SERVICE = PlantingService()
+SQL_SERVER = os.getenv('MYSQL_HOST')
+SQL_DATABASE = os.getenv('MYSQL_DATABASE')
+db_builder = SqlAlchemyBuilder(SQL_SERVER, SQL_DATABASE)
+SessionLocal = db_builder.get_session()
+
+
+@api_bp.route('/teste')
+def teste():
+    # lista = PlantingService.get_cultures(SessionLocal())
+    # print(lista)
+
+    lista = PlantingService.calc_and_register(SessionLocal(), 1, 500)
+    return jsonify(lista)
+
+
+    #
+    # try:
+    #     with SessionLocal() as session:
+    #         stmt = select(Culture)
+    #         result = session.scalars(stmt).all()
+    #
+    #         if not result:
+    #             return jsonify({
+    #                 'status': 'success',
+    #                 'message': "No cultures found",
+    #             })
+    #
+    #         culture_data = [
+    #             c.to_dict() if hasattr(c, 'to_dict') else {
+    #                 "id": c.id,
+    #                 "name": c.name,
+    #                 "product_id": c.product_id,
+    #                 "format_id": c.format_id,
+    #                 "street_size_m": c.street_size_m
+    #             }
+    #             for c in result
+    #         ]
+    #
+    #         return jsonify({
+    #             "status": "success",
+    #             "message": f"{len(culture_data)} cultures found",
+    #             "data": culture_data
+    #         })
+    # except Exception as e:
+    #     return jsonify({
+    #         'status': 'error',
+    #         'message': 'Query for cultures failed',
+    #         'detail': str(e)
+    #     })
 
 @api_bp.route('/calc-area', methods=['POST'])
 def calc_area():
     data = request.get_json()
+    print(data.get('culture'))
+    print('\n')
+    print('-'*20)
+    print(data.get('area'))
+
+    response = PlantingService.calc_and_register(
+        SessionLocal(),
+        data.get('culture'),
+        data.get('area')
+    )
+
+
+    return jsonify(response)
